2a06e14725acc5f45027174bb1628be8cf7b2275
aleskitta <alesyadamasevich@gmail.com>
2025-10-26 01:47:11 +0300

    Add tests for fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,14 +4,3 @@
 
 using namespace std;
 
-vector<unsigned long long> Fibonacci::generated(unsigned int n) {
-	if (n == 0) return {};
-	if (n > 94) throw overflow_error("Too large n: overflow risk");
-	vector<unsigned long long> result(n + 1);
-	result[0] = 1;
-	result[1] = 1;
-	for (int i = 2; i <= n; i++) {
-		result[i] = result[i - 1] + result[i - 2];
-	}
-	return result;
-}
--- fibonacci.h
+++ fibonacci.h
@@ -1,10 +1,24 @@
 #pragma once
 #include <vector>
+#include <stdexcept>
 
 using namespace std;
 
 class Fibonacci {
 public:
-	static vector<unsigned long long> generated(unsigned int n);
+	static vector<unsigned long long> generated(int n) {
+        if (n == 0) return {};
+        if (n > 94) throw overflow_error("Too large n: overflow risk");
+        vector<unsigned long long> result(n);
+        result.reserve(n);
+
+        if (n >= 1) result[0] = 0;
+        if (n >= 2) result[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result;
+	}
 };
 
