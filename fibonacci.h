6a1e639d7617c35d4f5281c4b91b07ef4d8ddd37
aleskitta <alesyadamasevich@gmail.com>
2025-10-25 17:36:57 +0300

    Implement Fibonacci with overflow checks

---
+++ fibonacci.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+class Fibonacci {
+public:
+	static vector<unsigned long long> generated(unsigned int n);
+};
+
--- task-OS.cpp
+++ task-OS.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "fibonacci.h"
+
+using namespace std;
+
+void demoFibonacci() {
+    cout << "Fibonacci Demo:" << endl;
+    try {
+        auto fibs = Fibonacci::generated(10);
+        cout << "First 10 Fibonacci numbers: ";
+        for (auto num : fibs) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    catch (const exception& e) {
+        cout << "Error: " << e.what() << endl;
+    }
+}
 
 int main() {
 
+    demoFibonacci();
 	return 0;
 }
